2a63c15ce2ac50ffffff0dfd04a6c2966092c8bf
ianuvion <ianuvion@gmail.com>
2025-10-25 01:38:17 -0300

    Create next.config.mjs
    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nuvion-logos.s3.us-east-1.amazonaws.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
