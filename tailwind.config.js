a21fb55923b01e132ec16a42c8597ecb32804dc8
ianuvion <ianuvion@gmail.com>
2025-10-28 13:34:25 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // 👈 esta línea es la que faltaba
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: { 
+    extend: {}, 
+  },
   plugins: [],
 };
