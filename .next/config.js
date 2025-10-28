c57bfcbb7a3accd63e81174da27ac07cbc646351
ianuvion <ianuvion@gmail.com>
2025-10-28 00:02:29 -0300

    Update config.js

--- .next/config.js
+++ .next/config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // patrón general para buckets de S3 (us-east-1)
-      { protocol: 'https', hostname: '*.s3.amazonaws.com' },
-      { protocol: 'https', hostname: '*.s3.*.amazonaws.com' },
-      // tu bucket directo (opcional, pero suma)
-      { protocol: 'https', hostname: 'nuvion-logos.s3.us-east-1.amazonaws.com' },
+      {
+        protocol: 'https',
+        hostname: 'nuvion-logos.s3.us-east-1.amazonaws.com',
+        pathname: '/logos/**',
+      },
     ],
   },
 };
