5b6b12173ef818f804b68b6fe3d465d8814742bc
ianuvion <ianuvion@gmail.com>
2025-10-29 22:18:57 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+import Navbar from "./components/Navbar";
 
-import type { Metadata } from "next";
-import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "Nuvion IA",
-  description: "Dashboard Nuvion IA",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
